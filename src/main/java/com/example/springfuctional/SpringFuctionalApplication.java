9864cb68e0fb51409156118b23a5cb9e6681a3cc
Aliaksandr Kazlouski <aliaksandr.kazlouski@netcracker.com>
2018-02-06 15:58:29 +0300

    first commit

---
+++ src/main/java/com/example/springfuctional/SpringFuctionalApplication.java
@@ -0,0 +1,12 @@
+package com.example.springfuctional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFuctionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFuctionalApplication.class, args);
+	}
+}
